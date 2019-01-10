ad3f65546ec67ace2cf4b768f553503854d1195e
daniloaqui <45565792+daniloaqui@users.noreply.github.com>
2019-01-10 01:41:53 -0200

    Add existing file

---
+++ README.md
@@ -0,0 +1,2 @@
+# LovusBrasil
+Lovus Brasil  um aplicativo de contedo adulto permitido para maiores de 18 anos, as categorias de anncios so ENCONTROS CASUAIS, CASAS NOTURNAS  BARES, MASSAGISTAS, HOTIS  MOTIS  
---
+++ www/js/app.js
@@ -0,0 +1,286 @@
+angular.module("lovusbrasil", ["ngCordova","ionic","ionMdInput","ionic-material","ion-datetime-picker","ionic.rating","utf8-base64","angular-md5","chart.js","pascalprecht.translate","tmh.dynamicLocale","lovusbrasil.controllers", "lovusbrasil.services"])
+	.run(function($ionicPlatform,$window,$interval,$timeout,$ionicHistory,$ionicPopup,$state,$rootScope){
+
+		$rootScope.appName = "Lovus Brasil" ;
+		$rootScope.appLogo = "data/images/header/logo.png" ;
+		$rootScope.appVersion = "1.0" ;
+		$rootScope.headerShrink = false ;
+
+		$rootScope.liveStatus = "pause" ;
+		$ionicPlatform.ready(function(){
+			$rootScope.liveStatus = "run" ;
+		});
+		$ionicPlatform.on("pause",function(){
+			$rootScope.liveStatus = "pause" ;
+		});
+		$ionicPlatform.on("resume",function(){
+			$rootScope.liveStatus = "run" ;
+		});
+
+
+		$rootScope.hide_menu_about_us = false ;
+		$rootScope.hide_menu_rate_this_app = false ;
+
+
+		$ionicPlatform.ready(function() {
+
+			localforage.config({
+				driver : [localforage.WEBSQL,localforage.INDEXEDDB,localforage.LOCALSTORAGE],
+				name : "lovusbrasil",
+				storeName : "lovusbrasil",
+				description : "The offline datastore for LovusBrasil app"
+			});
+
+			if(window.cordova){
+				$rootScope.exist_cordova = true ;
+			}else{
+				$rootScope.exist_cordova = false ;
+			}
+			//required: cordova plugin add ionic-plugin-keyboard --save
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			//required: cordova plugin add cordova-plugin-statusbar --save
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+
+			//required: cordova plugin add onesignal-cordova-plugin --save
+			if(window.plugins && window.plugins.OneSignal){
+				window.plugins.OneSignal.enableNotificationsWhenActive(true);
+				var notificationOpenedCallback = function(jsonData){
+					try {
+						$timeout(function(){
+							$window.location = "#/lovusbrasil/" + jsonData.notification.payload.additionalData.page ;
+						},200);
+					} catch(e){
+						console.log("onesignal:" + e);
+					}
+				}
+				window.plugins.OneSignal.startInit("187082b8-dda3-432d-8f09-41abdfe3f38e").handleNotificationOpened(notificationOpenedCallback).endInit();
+			}
+
+
+		});
+		$ionicPlatform.registerBackButtonAction(function (e){
+			if($ionicHistory.backView()){
+				$ionicHistory.goBack();
+			}else{
+				$state.go("lovusbrasil.inicio");
+			}
+			e.preventDefault();
+			return false;
+		},101);
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+	.filter("trustUrl", function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	})
+
+	.filter("trustJs", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsJs(text);
+		};
+	}])
+
+	.filter("strExplode", function() {
+		return function($string,$delimiter) {
+			if(!$string.length ) return;
+			var $_delimiter = $delimiter || "|";
+			return $string.split($_delimiter);
+		};
+	})
+
+	.filter("strDate", function(){
+		return function (input) {
+			return new Date(input);
+		}
+	})
+	.filter("phpTime", function(){
+		return function (input) {
+			var timeStamp = parseInt(input) * 1000;
+			return timeStamp ;
+		}
+	})
+	.filter("strHTML", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+	.filter("strEscape",function(){
+		return window.encodeURIComponent;
+	})
+	.filter("strUnscape", ["$sce", function($sce) {
+		var div = document.createElement("div");
+		return function(text) {
+			div.innerHTML = text;
+			return $sce.trustAsHtml(div.textContent);
+		};
+	}])
+
+	.filter("stripTags", ["$sce", function($sce){
+		return function(text) {
+			return text.replace(/(<([^>]+)>)/ig,"");
+		};
+	}])
+
+	.filter("chartData", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if ((indeks !== 0) && (indeks !== 1)){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+	.filter("chartLabels", function(){
+		return function (obj){
+			var new_item = [];
+			angular.forEach(obj, function(child) {
+			var indeks = 0;
+			new_item = [];
+			angular.forEach(child, function(v,l) {
+				if ((indeks !== 0) && (indeks !== 1)) {
+					new_item.push(l);
+				}
+				indeks++;
+			});
+			});
+			return new_item;
+		}
+	})
+	.filter("chartSeries", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if (indeks === 1){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+
+
+.config(["$translateProvider", function ($translateProvider){
+	$translateProvider.preferredLanguage("pt-br");
+	$translateProvider.useStaticFilesLoader({
+		prefix: "translations/",
+		suffix: ".json"
+	});
+	$translateProvider.useSanitizeValueStrategy("escapeParameters");
+}])
+
+
+.config(function(tmhDynamicLocaleProvider){
+	tmhDynamicLocaleProvider.localeLocationPattern("lib/ionic/js/i18n/angular-locale_{{locale}}.js");
+	tmhDynamicLocaleProvider.defaultLocale("pt-br");
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider,$httpProvider,$ionicConfigProvider){
+	try{
+		// Domain Whitelist
+		$sceDelegateProvider.resourceUrlWhitelist([
+			"self",
+			new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?w3schools\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?lovus\.com\.br/.+$'),
+		]);
+	}catch(err){
+		console.log("%cerror: %cdomain whitelist","color:blue;font-size:16px;","color:red;font-size:16px;");
+	}
+	$stateProvider
+	.state("lovusbrasil",{
+		url: "/lovusbrasil",
+			abstract: true,
+			templateUrl: "templates/lovusbrasil-side_menus.html",
+			controller: "side_menusCtrl",
+	})
+
+	.state("lovusbrasil.about_us", {
+		url: "/about_us",
+		views: {
+			"lovusbrasil-side_menus" : {
+						templateUrl:"templates/lovusbrasil-about_us.html",
+						controller: "about_usCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("lovusbrasil.dashboard", {
+		url: "/dashboard",
+		views: {
+			"lovusbrasil-side_menus" : {
+						templateUrl:"templates/lovusbrasil-dashboard.html",
+						controller: "dashboardCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("lovusbrasil.inicio", {
+		url: "/inicio",
+		views: {
+			"lovusbrasil-side_menus" : {
+						templateUrl:"templates/lovusbrasil-inicio.html",
+						controller: "inicioCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("lovusbrasil.slide_tab_menu", {
+		url: "/slide_tab_menu",
+		views: {
+			"lovusbrasil-side_menus" : {
+						templateUrl:"templates/lovusbrasil-slide_tab_menu.html",
+						controller: "slide_tab_menuCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+
+// router by user
+
+
+	$urlRouterProvider.otherwise("/lovusbrasil/inicio");
+});
